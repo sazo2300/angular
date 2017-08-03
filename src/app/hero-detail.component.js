7f6893acad4a1a514655570d924ec913dc455f2c
Hazem Saeid (Copenhagen Business School) <b-hasaei@microsoft.com>
2017-08-03 13:51:18 +0200

    done

--- src/app/hero-detail.component.js
+++ src/app/hero-detail.component.js
@@ -9,10 +9,31 @@ var __metadata = (this && this.__metadata) || function (k, v) {
     if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
 };
 var core_1 = require("@angular/core");
+var router_1 = require("@angular/router");
+var common_1 = require("@angular/common");
+require("rxjs/add/operator/switchMap");
+var hero_service_1 = require("./hero.service");
 var hero_1 = require("./hero");
 var HeroDetailComponent = (function () {
-    function HeroDetailComponent() {
+    function HeroDetailComponent(heroService, route, location) {
+        this.heroService = heroService;
+        this.route = route;
+        this.location = location;
     }
+    HeroDetailComponent.prototype.ngOnInit = function () {
+        var _this = this;
+        this.route.paramMap
+            .switchMap(function (params) { return _this.heroService.getHero(+params.get('id')); })
+            .subscribe(function (hero) { return _this.hero = hero; });
+    };
+    HeroDetailComponent.prototype.goBack = function () {
+        this.location.back();
+    };
+    HeroDetailComponent.prototype.save = function () {
+        var _this = this;
+        this.heroService.update(this.hero)
+            .then(function () { return _this.goBack(); });
+    };
     return HeroDetailComponent;
 }());
 __decorate([
@@ -22,8 +43,12 @@ __decorate([
 HeroDetailComponent = __decorate([
     core_1.Component({
         selector: 'hero-detail',
-        template: "<div *ngIf=\"hero\">\n        <h2>{{hero.name}} details!</h2>\n    <div><label>id: </label>{{hero.id}}</div>\n        <label>name: </label>{{hero.name}}\n        <input [(ngModel)]=\"hero.name\" placeholder=\"name\">\n  </div>"
-    })
+        templateUrl: './hero-detail.component.html',
+        styleUrls: ['./hero-detail.component.css']
+    }),
+    __metadata("design:paramtypes", [hero_service_1.HeroService,
+        router_1.ActivatedRoute,
+        common_1.Location])
 ], HeroDetailComponent);
 exports.HeroDetailComponent = HeroDetailComponent;
 //# sourceMappingURL=hero-detail.component.js.map
--- src/app/hero-search.service.js
+++ src/app/hero-search.service.js
@@ -9,25 +9,22 @@ var __metadata = (this && this.__metadata) || function (k, v) {
     if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
 };
 var core_1 = require("@angular/core");
-var hero_service_1 = require("./hero.service");
-var DashboardComponent = (function () {
-    function DashboardComponent(heroService) {
-        this.heroService = heroService;
-        this.heroes = [];
+var http_1 = require("@angular/http");
+require("rxjs/add/operator/map");
+var HeroSearchService = (function () {
+    function HeroSearchService(http) {
+        this.http = http;
     }
-    DashboardComponent.prototype.ngOnInit = function () {
-        var _this = this;
-        this.heroService.getHeroes().then(function (heroes) { return _this.heroes = heroes.slice(1, 5); });
+    HeroSearchService.prototype.search = function (term) {
+        return this.http
+            .get("api/heroes/?name=" + term)
+            .map(function (response) { return response.json().data; });
     };
-    return DashboardComponent;
+    return HeroSearchService;
 }());
-DashboardComponent = __decorate([
-    core_1.Component({
-        selector: 'my-dashboard',
-        templateUrl: './dashboard.component.html',
-        styleUrls: ['./dashboard.component.css']
-    }),
-    __metadata("design:paramtypes", [hero_service_1.HeroService])
-], DashboardComponent);
-exports.DashboardComponent = DashboardComponent;
-//# sourceMappingURL=dashboard.component.js.map
+HeroSearchService = __decorate([
+    core_1.Injectable(),
+    __metadata("design:paramtypes", [http_1.Http])
+], HeroSearchService);
+exports.HeroSearchService = HeroSearchService;
+//# sourceMappingURL=hero-search.service.js.map
